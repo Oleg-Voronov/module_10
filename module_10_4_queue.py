c38f8ffbedc721e6fcc2a5aba919140af54c6e35
Oleg-Voronov <vollmihh@gmail.com>
2024-12-31 00:38:12 +0800

    Update module_10_4_queue.py

--- module_10_4_queue.py
+++ module_10_4_queue.py
@@ -51,12 +51,10 @@ class Cafe:
                     print(f'{t.guest.name} покушал(-а) и ушёл(ушла)')
                     print(f'Стол номер {t.number} свободен.')
                     t.guest = None
-            for t in self.tables:
-                if t.guest == None and not self.que.empty():
-                    t.guest = self.que.get()
-                    t.guest.start()
-                    print(f'{t.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {t.number}')
-
+                    if not self.que.empty():
+                        t.guest = self.que.get()
+                        t.guest.start()
+                        print(f'{t.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {t.number}')
 
 tables = [Table(number) for number in range(1, N_)]
 guests_names = ['Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
